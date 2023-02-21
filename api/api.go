850b87fa92f60d66bcf79c958f29e8f341d33238
Axel Schuster <axel_schuster_@gmx.net>
2023-02-21 23:49:06 +0100

    Adjusting options of the TTQL CLI, introducing hack for any()
    Makes the appearance of the TTQL CLI more mature. Also a hack
    is introduced so we can use "WHERE any(a.prop > 20)" because it is more
    intuitive. Under the hood it is still the same as writing
    "WHERE a.prop > 20" though. This should be changed in the future.

--- api/api.go
+++ api/api.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
+	"github.com/LexaTRex/timetravelDB/utils"
 	"github.com/c-bata/go-prompt"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
@@ -75,7 +77,7 @@ func Api() {
 	// ####
 	// #### QUERIES NACH CASES: siehe schaubild bzw den condition tree mit den checkpoints (prints)
 	// #### queries case 1:
-	//    FROM 2021-12-22T15:33:13.0000005Z TO 2024-01-12T15:33:13.0000006Z SHALLOW MATCH (a)-[x]->(b) WHERE a.properties_components_cpu = 'UGWJn' RETURN  * | should return 2 match (a)-[x]->(b) with a.properties_components_cpu = 'UGWJn'
+	//    FROM 2021-12-22T15:33:13.0000005Z TO 2024-01-12T15:33:13.0000006Z SHALLOW MATCH (a)-[x]->(b) WHERE a.properties_components_cpu = 'UGWJn' RETURN  * | should return 2 match (a)-[x]->(b) with a.properties_components_cpu = 'UGWJn' (but not fetchted)
 	//																																																																												 because the node with a.properties_components_cpu = 'UGWJn' is occouring in a pattern like this twice
 	//    FROM 2021-12-22T15:33:13.0000005Z TO 2024-01-12T15:33:13.0000006Z SHALLOW MATCH (a)-[x]->(b) WHERE a.properties_components_cpu = 'not available' RETURN  * | should return nothing
 	//    FROM 2021-12-22T15:33:13.0000005Z TO 2024-01-12T15:33:13.0000006Z SHALLOW MATCH (a)-[x]->(b) WHERE 'aa' = a.properties_components_cpu RETURN  * | should return nothing
@@ -95,14 +97,23 @@ func Api() {
 
 	// THE LAST ONE IS THE ONLY ONE NOT WORKING YET
 
+	// QUERIES NON SHALLOW
+	// FROM 2021-12-22T15:33:13.0000005Z TO 2024-01-12T15:33:13.0000006Z  MATCH (a)-[x]->(b) RETURN  *
+	// FROM 2021-12-22T15:33:13.0000005Z TO 2024-01-12T15:33:13.0000006Z  MATCH (a)-[x]->(b) WHERE a.properties_components_cpu = 'UGWJn' RETURN  *
+	// FROM 2021-12-22T15:33:13.0000005Z TO 2024-01-12T15:33:13.0000006Z  MATCH (a)-[x]->(b) WHERE a.properties_components_cpu = 'UGWJn' RETURN  *
+
 	fmt.Print("Enter Query: \n")
 	p := prompt.New(
 		executor,
 		completer,
 		prompt.OptionPrefix("TTQL> "),
 		prompt.OptionTitle("TTQL CLI"),
+		prompt.OptionDescriptionBGColor(prompt.DarkGray),
+		prompt.OptionPreviewSuggestionTextColor(prompt.Black),
+		prompt.OptionSelectedSuggestionBGColor(prompt.LightGray),
+		prompt.OptionSelectedDescriptionBGColor(prompt.DarkGray),
+		prompt.OptionSuggestionBGColor(prompt.DarkGray),
 	)
-
 	p.Run()
 
 }
@@ -128,7 +139,9 @@ func executor(in string) {
 		fmt.Println("To query TimeTravelDB type in a valid TTQL query.\nTo exit the program type 'quit' or 'exit'.\nFor more infos: https://github.com/LexaTRex/timetravelDB/")
 	default:
 		fmt.Printf("Processing Query: %s\n", in)
-		err := ProcessQuery(in)
+		in = cleanQuery(in)
+		res, err := ProcessQuery(in)
+		utils.UNUSED(res)
 		if err != nil {
 			log.Printf("Failed: %v", err)
 		}
@@ -137,7 +150,8 @@ func executor(in string) {
 
 // auto completion suggestions for the prompt
 func completer(in prompt.Document) []prompt.Suggest {
-	return []prompt.Suggest{{Text: "FROM", Description: "FROM"}, {Text: "TO", Description: "TO"}, {Text: "SHALLOW", Description: "SHALLOW"}, {Text: "MATCH", Description: "MATCH"}, {Text: "WHERE", Description: "WHERE"}, {Text: "RETURN", Description: "RETURN"}}
+	p := []prompt.Suggest{{Text: "FROM"}, {Text: "TO"}, {Text: "SHALLOW"}, {Text: "MATCH"}, {Text: "WHERE"}, {Text: "RETURN"}}
+	return prompt.FilterHasPrefix(p, in.GetWordBeforeCursor(), true)
 }
 
 // workaround to get the terminal back to normal after exiting the program
@@ -146,4 +160,34 @@ func handleExit() {
 	rawModeOff.Stdin = os.Stdin
 	_ = rawModeOff.Run()
 	rawModeOff.Wait()
+}
+
+// this is a dirty hack which just gives the illusion that we can use any() in the WHERE clause
+// like any(a.prop) > 23. The functionality is working but parser wise it basically is the same
+// as a.prop > 23. This is just for convinience and better usability. Change this in the future.
+// Disallow a.prop > 23 and only allow any(a.prop) > 23 in changing the TTQL grammar accordingly.
+func cleanQuery(query string) string {
+	// Split the query into three parts: MATCH, WHERE, and RETURN
+	parts := strings.Split(query, "WHERE")
+	if len(parts) != 2 {
+		// Query doesn't have a WHERE clause, return the original query
+		return query
+	}
+
+	matchClause := parts[0]
+	whereClause := parts[1]
+	returnClause := ""
+	returnIndex := strings.Index(whereClause, "RETURN")
+	if returnIndex != -1 {
+		returnClause = whereClause[returnIndex:]
+		whereClause = whereClause[:returnIndex]
+	}
+
+	// Replace any() with a.prop
+	whereClause = strings.ReplaceAll(whereClause, "any(", "")
+	whereClause = strings.ReplaceAll(whereClause, ")", "")
+
+	// Reassemble the query
+	query = matchClause + "WHERE " + whereClause + returnClause
+	return query
 }
