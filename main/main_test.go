3951318c0e4150e774bc12c87692f72b1039f601
Axel Schuster <axel_schuster_@gmx.net>
2023-02-27 16:27:10 +0100

    fixup test automatisation

---
+++ main/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+// func TestIntegration(t *testing.T) {
+// 	cmd := exec.Command("/bin/bash", "-c", "sudo ../run_integration_test.sh")
+// 	err := cmd.Run()
+// 	if err != nil {
+// 		t.Errorf("Failed to run integration test: %v", err)
+// 	}
+// }
+
+func TestIntegration(t *testing.T) {
+	cmd := exec.Command("/bin/bash", "../run_integration_test.sh")
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = io.MultiWriter(&stdout, os.Stdout)
+	cmd.Stderr = io.MultiWriter(&stderr, os.Stderr)
+	err := cmd.Run()
+	if err != nil {
+		t.Errorf("Failed to run integration test: %v\n%s", err, stderr.String())
+	}
+	fmt.Printf("%s", stdout.String())
+}
