25a3e65c10397e054cdba37fb32023bfd1fa0c1a
Axel Schuster <axel_schuster_@gmx.net>
2023-03-09 14:21:23 +0100

    New generated test-data, merged helper files

--- utils/helpers.go
+++ utils/helpers.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -336,4 +337,35 @@ func DebugIn(str ...interface{}) {
 		}
 		return
 	}
+}
+
+// helpers imported from Data Loader
+
+func LoadJsonData(path string) ([]map[string]interface{}, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Decode the JSON data
+	var data []map[string]interface{}
+	err = json.NewDecoder(file).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func ConvertMaps(originalMaps []interface{}) []map[string]interface{} {
+	convertedMaps := make([]map[string]interface{}, 0)
+	for _, originalMap := range originalMaps {
+		convertedMap := map[string]interface{}{}
+		for key, value := range originalMap.(map[string]interface{}) {
+			convertedMap[key] = value.(interface{})
+		}
+		convertedMaps = append(convertedMaps, convertedMap)
+	}
+	return convertedMaps
 }
