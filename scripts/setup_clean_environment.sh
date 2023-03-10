300551bb14d8c6d451268a21ff39c260f050639c
Axel Schuster <axel_schuster_@gmx.net>
2023-03-10 12:22:10 +0100

    Fix testing script, test main_test.go still not working
    It seems to be a problem to get main_test.go to work because
    of the neo4j restore which seems to make problems if run
    through a secondary call. Could not figure out why yet
    but seems to have something to do with the terminal
    environment.

---
+++ scripts/setup_clean_environment.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This file sets up an testing environment providing a docker Neo4j DMBS and a docker TimescaleDB DMBS which run databases with 
+# simplistic testing data 
+
+SCRIPT=$(realpath "$0")
+TTDB_SCRIPTS=$(dirname "$SCRIPT")
+
+# stop all running docker containers to avoid conflicts 
+docker stop $(docker ps -aq)
+
+# cleanup before (if docker-compose was run in other ways than this script there might be some leftovers)
+sudo rm -rf $TTDB_SCRIPTS/../docker-test/neo4j/backups/*
+sudo rm -rf $TTDB_SCRIPTS/../docker-test/neo4j/data/*
+sudo rm -rf $TTDB_SCRIPTS/../docker-test/timescaledb/backups/*
+sudo rm -rf $TTDB_SCRIPTS/../docker-test/timescaledb/data/*
+
+
+# prepare testing envionment 
+docker-compose -f $TTDB_SCRIPTS/../docker-compose.yml up -d
+
+# wait for testing envionment to be ready 
+dockerize -wait tcp://127.0.0.1:7687 -timeout 10s
+dockerize -wait tcp://127.0.0.1:5432 -timeout 10s
